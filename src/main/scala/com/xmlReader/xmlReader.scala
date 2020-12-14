1f33da0ae1b9e2539b6ff7eadd18975a59f33172
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-12-14 15:53:04 +0530

    sync

--- src/main/scala/com/xmlReader/xmlReader.scala
+++ src/main/scala/com/xmlReader/xmlReader.scala
@@ -3,6 +3,12 @@ package com.xmlReader
 import com.databricks.spark.xml.XmlDataFrameReader
 import org.apache.spark.sql.SparkSession
 
+
+// for XML parsing
+//libraryDependencies += "com.databricks" %% "spark-xml" % "0.5.0"
+//dependencyOverrides += "com.google.guava" % "guava" % "15.0"
+
+
 object xmlReader
 {
     val spark: SparkSession = SparkSession
