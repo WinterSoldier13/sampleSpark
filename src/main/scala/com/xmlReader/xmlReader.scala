82ec3c1d1bf88ad7a9d8cf4faad655dc782902ee
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-12-14 15:51:59 +0530

    added XML reader

--- src/main/scala/com/xmlReader/xmlReader.scala
+++ src/main/scala/com/xmlReader/xmlReader.scala
@@ -1,6 +1,28 @@
 package com.xmlReader
 
+import com.databricks.spark.xml.XmlDataFrameReader
+import org.apache.spark.sql.SparkSession
+
 object xmlReader
 {
+    val spark: SparkSession = SparkSession
+        .builder()
+        .appName("sampleApp")
+        .master("local[*]")
+        .getOrCreate()
+    import spark.implicits._
     
+    def main(args : Array[String]): Unit =
+    {
+        val df = spark
+            .read
+            .format("com.databricks.spark.xml")
+            .option("rowTag", "CofiResults")
+            .xml("src/main/static/dataset/sampleXML.xml")
+        
+        df.printSchema()
+        df.show(10)
+        
+        spark.close()
+    }
 }
